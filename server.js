45610c5f606bb10e2425e9e485830cf275a08c68
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-12-18 22:24:31 +0100

    Connection error

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const socket = require('socket.io');
 
 
 const tasks = [];
 
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
+app.use(express.static(path.join(__dirname, '/client/public')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/public/index.html'));
+});
 
 const server = app.listen(process.env.PORT || 8000, () => {
     console.log('Server is running on port: 8000');
