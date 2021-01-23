f0d292d665f4ca10950728e1ae5a46e5b38d8401
Ewa Witkowska <ewa.witkowska09@gmail.com>
2021-01-23 19:21:33 +0100

    Add adding and removing list item

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,43 @@ import io from 'socket.io-client';
 
 
 class App extends React.Component {
+  state = {
+    tasks: [{id: 0, name: 'Do something'}],
+    taskName: '',
+  }
 
   componentDidMount = () => {
     this.socket = io.connect('localhost:8000');
   }  
 
+  removeTask (id) {
+    this.setState({
+      ...this.state,
+      tasks: this.state.tasks.filter(task => task.is === id)
+    });
+    this.socket.emit('removeTask', id);
+  }
+
+  updateTaskName (event) {
+    this.setState({
+      ...this.state,
+      taskName: event.target.value,
+    })
+  }
+
+  submitForm (event) {
+    event.preventDefault();
+    this.addTask(this.state.taskName);
+    this.socket.emit('addTask', this.state.taskName);
+  }
+
+    addTask(task) {
+      this.setState(state =>({
+        ...state,
+        tasks: [...state.tasks, {id: task.id, taskName: task.name}]
+      }));
+    }
+  
 
   render() {
 
@@ -22,13 +54,14 @@ class App extends React.Component {
       <h2>Tasks</h2>
 
       <ul className="tasks-section__list" id="tasks-list">
-        <li class="task">Shopping <button class="btn btn--red">Remove</button></li>
-        <li class="task">Go out with a dog <button class="btn btn--red">Remove</button></li>
+        {this.state.tasks.map( (task)  => (
+          <li key={task.id} className="task"> {task.name} <button className="btn btn--red" onClick={()=> this.removeTask(task.id)} >Remove</button></li>
+        ) )}
       </ul>
 
       <form id="add-task-form">
-        <input className="text-input" autocomplete="off" type="text" placeholder="Type your description" id="task-name" />
-        <button className="btn" type="submit">Add</button>
+        <input className="text-input" autoComplete="off" type="text" placeholder="Type your description" id="task-name" value={this.state.taskName} onChange={(event) => this.updateTaskName(event)}/>
+        <button className="btn" type="submit" onClick={(event) => this.submitForm(event)}>Add</button>
       </form>
     </section>
   </div>
